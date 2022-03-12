f122100d2c3680a0a761a2c2c5ae0fcad938d13e
Alek <alek.jakimovski1@gmail.com>
2022-03-12 18:11:02 +0100

    projects scaffolding

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import styles from "./Projects.module.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Projects = () => {
     return (
         <div className={styles["p"]}>
+            <div className={styles["p-wrapper"]}>
+                <h2 className={styles["p-title"]}>My Projects</h2>
+                <div className={styles["p-divide"]}></div>
+                <p className={styles["p-text"]}>Check out some of the stuff I've made</p>
+            </div>
+
+            <div className={styles["p-project-wrap"]}>
+                <Carousel>
+                    <div>
+                        <p>Legend 1</p>
+                    </div>
+                    <div>
+                        <p>Legend 2</p>
+                    </div>
+                    <div>
+                        <p>Legend 3</p>
+                    </div>
+                </Carousel>
+            </div>
 
         </div>
     )
