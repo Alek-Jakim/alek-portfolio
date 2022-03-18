dc62d5ab460631f980a1b0ac6d0033bf31e89bf4
Alek <alek.jakimovski1@gmail.com>
2022-03-18 23:12:49 +0100

    refactor handleSidebar

--- src/components/Scroll/Scroll.tsx
+++ src/components/Scroll/Scroll.tsx
@@ -13,21 +13,29 @@ const Scroll = () => {
 
         let burgerIcon: any = document.querySelector(".Scroll_s-burger__HChR-");
 
+        let intro: any = document.getElementById("s-intro");
+        let about: any = document.getElementById("s-about");
+        let projects: any = document.getElementById("s-projects");
+        let contact: any = document.getElementById("s-contact");
+
+
+
         let sideItem = document.querySelector(".Scroll_s__9nuif")?.children;
         let arr: any = [].slice.call(sideItem);
         arr.shift();
 
         if (!open) {
-            arr.forEach((item: any) => {
-                item.style.left = "-5px";
-            });
+            intro.style.left = "-10px";
+            about.style.left = "-10px";
+            projects.style.left = "-10px";
+            contact.style.left = "-10px";
             setOpen(true);
             burgerIcon.style.transform = "rotate(180deg)";
-            console.log(burgerIcon)
         } else {
-            arr.forEach((item: any) => {
-                item.style.left = "-100px"
-            });
+            intro.style.left = "-100px";
+            about.style.left = "-100px";
+            projects.style.left = "-100px";
+            contact.style.left = "-100px";
             burgerIcon.style.transform = "rotate(360deg)";
             setOpen(false);
         }
@@ -41,25 +49,25 @@ const Scroll = () => {
 
 
 
-            <div className={styles["s-intro"]}>
+            <div className={styles["s-intro"]} id="s-intro">
                 <Link to="intro" smooth={true} className={styles["s-link"]}>
                     <AiFillFire className={styles["s-icon"]} />
                 </Link>
             </div>
 
-            <div className={styles["s-about"]}>
+            <div className={styles["s-about"]} id="s-about">
                 <Link to="about" smooth={true} className={styles["s-link"]}>
                     <AiTwotoneSetting className={styles["s-icon"]} />
                 </Link>
             </div>
 
-            <div className={styles["s-projects"]}>
+            <div className={styles["s-projects"]} id="s-projects">
                 <Link to="projects" smooth={true} className={styles["s-link"]}>
                     <AiFillBulb className={styles["s-icon"]} />
                 </Link>
             </div>
 
-            <div className={styles["s-contact"]}>
+            <div className={styles["s-contact"]} id="s-contact">
                 <Link to="contact" smooth={true} className={styles["s-link"]}>
                     <AiFillPhone className={styles["s-icon"]} />
                 </Link>
